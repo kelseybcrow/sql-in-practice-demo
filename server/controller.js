f90dc9e3fa2b442ee1a53fb4f2bde038a421ebe4
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-05 10:44:05 -0700

    finished demo

--- server/controller.js
+++ server/controller.js
@@ -1,49 +1,49 @@
-const { CONNECTION_STRING } = process.env;
-const { user } = require("pg/lib/defaults");
-const Sequelize = require("sequelize");
+const { CONNECTION_STRING } = process.env
+const { user } = require("pg/lib/defaults")
+const Sequelize = require("sequelize")
 
 const sequelize = new Sequelize(CONNECTION_STRING, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-});
+	dialect: "postgres",
+	dialectOptions: {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
+})
 
-const userId = 4;
-const clientId = 3;
+const userId = 4
+const clientId = 3
 
 module.exports = {
-  getUserInfo: (req, res) => {
-    sequelize
-      .query(
-        `
+	getUserInfo: (req, res) => {
+		sequelize
+			.query(
+				`
       SELECT * FROM cc_clients c
       JOIN cc_users u ON c.user_id = u.user_id
       WHERE u.user_id = ${userId};
     `
-      )
-      .then((dbRes) => {
-        // console.log(dbRes[0]);
-        res.status(200).send(dbRes[0]);
-      })
-      .catch((err) => console.log(err));
-  },
-  updateUserInfo: (req, res) => {
-    const {
-      firstName,
-      lastName,
-      phoneNumber,
-      email,
-      address,
-      city,
-      state,
-      zipCode,
-    } = req.body;
-    sequelize
-      .query(
-        `
+			)
+			.then((dbRes) => {
+				// console.log(dbRes[0]);
+				res.status(200).send(dbRes[0])
+			})
+			.catch((err) => console.log(err))
+	},
+	updateUserInfo: (req, res) => {
+		const {
+			firstName,
+			lastName,
+			phoneNumber,
+			email,
+			address,
+			city,
+			state,
+			zipCode,
+		} = req.body
+		sequelize
+			.query(
+				`
       UPDATE cc_users
       SET first_name = '${firstName}',
       last_name = '${lastName}',
@@ -58,20 +58,35 @@ module.exports = {
       zip_code = ${zipCode}
       WHERE user_id = ${userId};
     `
-      )
-      .then(() => res.sendStatus(200))
-      .catch((err) => console.log(err));
-  },
-  getUserAppt: (req, res) => {
-    sequelize
-      .query(
-        `
+			)
+			.then(() => res.sendStatus(200))
+			.catch((err) => console.log(err))
+	},
+	getUserAppt: (req, res) => {
+		sequelize
+			.query(
+				`
       SELECT * FROM cc_appointments
       WHERE client_id = ${clientId}
       ORDER BY date DESC;
     `
-      )
-      .then((dbRes) => res.status(200).send(dbRes[0]))
-      .catch((err) => console.log(err));
-  },
-};
+			)
+			.then((dbRes) => res.status(200).send(dbRes[0]))
+			.catch((err) => console.log(err))
+	},
+	requestAppointment: (req, res) => {
+		console.log(req.body)
+		const { date, service } = req.body
+
+		sequelize
+			.query(
+				`
+      INSERT INTO cc_appointments (client_id, date, service_type, notes, approved, completed)
+      VALUES (${clientId}, '${date}', '${service}', '', false, false)
+      RETURNING *;
+    `
+			)
+			.then((dbRes) => res.status(200).send(dbRes[0]))
+			.catch((err) => console.log(err))
+	},
+}
